6cb33790c29c57211e23162869428e69f3c05094
Terry O'Shea <terrykoshea@gmail.com>
2019-04-08 13:27:06 -0400

    fix vector function exports + add crossProduct function

--- src/vector.js
+++ src/vector.js
@@ -2,23 +2,25 @@ export const UP = { x: 0, y: 1, z: 0 };
 export const ZERO = { x: 0, y: 0, z: 0 };
 export const WHITE = { x: 255, y: 255, z: 255 };
 
-const dotProduct = (a, b) => ({
+export const dotProduct = (a, b) => a.x * b.x + a.y * b.y + a.z * b.z;
+
+export const crossProduct = (a, b) => ({
     x: a.y * b.z - a.z * b.y,
     y: a.z * b.x - a.x * b.z,
     z: a.x * b.y - a.y * b.x
 });
 
-const scale = (a, t) => ({
+export const scale = (a, t) => ({
     x: a.x * t,
     y: a.y * t,
     z: a.z * t
 });
 
-const length = a => Math.sqrt(dotProduct(a, a));
+export const length = a => Math.sqrt(dotProduct(a, a));
 
-const unitVector = a => scale(a, 1 / length(a));
+export const unitVector = a => scale(a, 1 / length(a));
 
-const add = (...vectors) => {
+export const add = (...vectors) => {
     const added = { x: 0, y: 0, z: 0 };
 
     vectors.forEach(vector => {
@@ -30,13 +32,13 @@ const add = (...vectors) => {
     return added;
 };
 
-const subtract = (a, b) => ({
+export const subtract = (a, b) => ({
     x: a.x - b.x,
     y: a.y - b.y,
     z: a.z - b.z
 });
 
-const reflectThrough = (a, normal) => {
+export const reflectThrough = (a, normal) => {
     const d = scale(normal, dotProduct(a, normal));
     return subtract(scale(d, 2), a);
 };
