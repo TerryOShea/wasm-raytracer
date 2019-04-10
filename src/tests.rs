5633c22130b25841deab18c6198aeb5238a67eb0
Terry O'Shea <terrykoshea@gmail.com>
2019-04-10 03:05:04 -0400

    add some more rust testing

--- src/tests.rs
+++ src/tests.rs
@@ -1,8 +1,88 @@
 use super::*;
 
+impl PartialEq for XYZ {
+    fn eq(&self, other: &XYZ) -> bool {
+        self.x == other.x && self.y == other.y && self.z == other.z
+    }
+}
+
 #[test]
-fn test_is_light_visible() {
-    assert!(true);
+fn test_scale() {
+    let vector = XYZ {
+        x: 4.0,
+        y: 5.0,
+        z: 6.0,
+    };
+    assert_eq!(
+        scale(&vector, 2.0),
+        XYZ {
+            x: 8.0,
+            y: 10.0,
+            z: 12.0
+        },
+    );
+}
+
+#[test]
+fn test_cross_product() {
+    let vector_1 = XYZ {
+        x: 4.0,
+        y: 5.0,
+        z: 6.0,
+    };
+    let vector_2 = XYZ {
+        x: 9.0,
+        y: 8.0,
+        z: 7.0,
+    };
+
+    assert_eq!(
+        cross_product(&vector_1, &vector_2),
+        XYZ {
+            x: -13.0,
+            y: 26.0,
+            z: -13.0
+        },
+    );
+}
+
+#[test]
+fn test_subtract() {
+    let vector_1 = XYZ {
+        x: 4.0,
+        y: 5.0,
+        z: 6.0,
+    };
+    let vector_2 = XYZ {
+        x: 9.0,
+        y: 8.0,
+        z: 7.0,
+    };
+
+    assert_eq!(
+        subtract(&vector_2, &vector_1),
+        XYZ {
+            x: 5.0,
+            y: 3.0,
+            z: 1.0
+        },
+    );
+}
+
+#[test]
+fn test_dot_product() {
+    let vector_1 = XYZ {
+        x: 4.0,
+        y: 5.0,
+        z: 6.0,
+    };
+    let vector_2 = XYZ {
+        x: 9.0,
+        y: 8.0,
+        z: 7.0,
+    };
+
+    assert_eq!(dot_product(&vector_1, &vector_2), 118.0);
 }
 
 #[test]
