df8b3bd6c67c76d0434a24f19a8f325184457e3c
Terry O'Shea <terrykoshea@gmail.com>
2019-04-10 02:08:24 -0400

    try adding pkg to git

---
+++ pkg/README.md
@@ -0,0 +1,3 @@
+A raytracer that makes use of (Rust-generated) WASM.
+
+Following along with [this JS raytracer tutorial](https://tmcw.github.io/literate-raytracer/), then adapting it to use web assembly.
---
+++ pkg/wasm_raytracer_bg.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_xyz_free(a: number): void;
+export function __wbg_get_xyz_x(a: number): number;
+export function __wbg_set_xyz_x(a: number, b: number): void;
+export function __wbg_get_xyz_y(a: number): number;
+export function __wbg_set_xyz_y(a: number, b: number): void;
+export function __wbg_get_xyz_z(a: number): number;
+export function __wbg_set_xyz_z(a: number, b: number): void;
+export function generate_new_data(a: number, b: number, c: number): void;
+export function __wbindgen_global_argument_ptr(): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_free(a: number, b: number): void;
